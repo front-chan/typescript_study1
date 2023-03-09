51b240358f165c3b639249865e0bc05ec4b2ef02
chan <chan@Chan-MacBook-Air.local>
2023-03-09 22:15:23 +0900

    study JSDoc

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,1 @@
-const hello = () => "hi";
-
-class Block {
-    constructor(private data: string) {}
-    static hello() {
-        return "hi";
-    }
-}
-
-import { init, exit } from "myPackage";
-
-init({
-    url: "true",
-});
-
-exit(1);
-
-localStorage.clear();
+import { init, exit } from "./myPackage";
